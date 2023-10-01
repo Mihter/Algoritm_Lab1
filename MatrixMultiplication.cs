fd29feb2746c65c2011d3829bcb1e11d0eadb737
Kazmin Mihail <mihail.kazmin.04@mail.ru>
2023-10-01 20:43:57 +0500

    Fix 0.8 -complete II stage ---stage III -Kazmin (Coming soon) -Guckov (Coming soon)

--- MatrixMultiplication.cs
+++ MatrixMultiplication.cs
@@ -9,23 +9,50 @@ namespace AlgoritmLab1
         static int j = 0;
         public static void MatrixMul(long[,] matrixOne, long[,] matrixTwo, long[] argArray)
         {
-            long[,] outMatrix = new long[matrixTwo.GetLength(0), matrixOne.Rank];
+            /*вывод первых двух массивов
+            for(int visota = 0; visota < matrixOne.GetLength(1); visota++)
+            {
+                for(int dlina = 0; dlina < matrixOne.GetLength(0); dlina++)
+                {
+                    Console.Write(matrixOne[dlina, visota]+" ");
+                }
+                Console.Write("   ");
+                for (int dlina = 0; dlina < matrixOne.GetLength(0); dlina++)
+                {
+                    Console.Write(matrixTwo[dlina, visota] + " ");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine();*/
+
+            long[,] outMatrix = new long[matrixTwo.GetLength(0), matrixOne.GetLength(1)];
+
             //считываем время работы алгоритма
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            for (int outMatrixRank = 0; outMatrixRank < matrixOne.Rank; outMatrixRank++)
+            for (int outMatrixRank = 0; outMatrixRank < matrixOne.GetLength(1); outMatrixRank++)
             {
                 for (int outMatrixLength = 0; outMatrixLength < matrixTwo.GetLength(0); outMatrixLength++)
                 {
                     for (int fillerOne = 0; fillerOne < matrixOne.GetLength(0); fillerOne++)
                     {
-                        outMatrix[outMatrixLength, outMatrixRank] += matrixOne[fillerOne, outMatrixRank] + matrixTwo[outMatrixLength, fillerOne];
+                        outMatrix[outMatrixLength, outMatrixRank] = outMatrix[outMatrixLength, outMatrixRank] + matrixOne[fillerOne, outMatrixRank] * matrixTwo[outMatrixLength, fillerOne];
                     }
                 }
             }
 
             stopwatch.Stop();
+            /* вывод конечного массива
+            for (int visota = 0; visota < matrixOne.GetLength(1); visota++)
+            {
+                for (int dlina = 0; dlina < matrixOne.GetLength(0); dlina++)
+                {
+                    Console.Write(outMatrix[dlina, visota] + " ");
+                }
+                Console.WriteLine("");
+            }
+            */
 
             argArray[j] = stopwatch.ElapsedTicks;
             //компенсация выполнения 5 экспериментов, обнуление индекса для 2-5 массива тиков
